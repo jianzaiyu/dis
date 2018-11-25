e473ab61ce87f12af03b1fca00f815af3923aba3
GGs <122490906@qq.com>
2018-11-26 01:34:15 +0800

    first

--- service-support/src/main/java/cn/ce/servicesupport/config/SwaggerConfig.java
+++ service-support/src/main/java/cn/ce/servicesupport/config/SwaggerConfig.java
@@ -1,5 +1,8 @@
 package cn.ce.servicesupport.config;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -26,32 +29,36 @@ import java.util.List;
 @EnableSwagger2
 //@Profile({"dev", "local", "pre"})//在生产环境不开启
 public class SwaggerConfig {
+    @Value("${spring.application.name}")
+    private String appName;
 
     @Bean
     public Docket createRestApi() {
-        ParameterBuilder ticketPar = new ParameterBuilder();
-        List<Parameter> pars = new ArrayList<Parameter>();
-        ticketPar.name("ticket")
-                .description("user ticket")
-                .modelRef(new ModelRef("string"))
-                .parameterType("header")
-                .required(false)
-                .build(); //添加全局的header参数
-        pars.add(ticketPar.build());
+//        ParameterBuilder ticketPar = new ParameterBuilder();
+//        List<Parameter> pars = new ArrayList<Parameter>();
+//        ticketPar.name("ticket")
+//                .description("user ticket")
+//                .modelRef(new ModelRef("string"))
+//                .parameterType("header")
+//                .required(false)
+//                .build(); //添加全局的header参数
+//        pars.add(ticketPar.build());
 
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(apiInfo())
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("cn.ce"))
+                .apis(RequestHandlerSelectors.withClassAnnotation(Api.class))
+                .apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class))
                 .paths(PathSelectors.any())
-                .build()
-                .globalOperationParameters(pars);
+                .build();
+//                .globalOperationParameters(pars);
 
     }
 
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
-                .title("DIS平台接口文档")
+                .title(appName + "接口文档")
 //                .description("restful风格接口")
 //                .contact(new Contact("ggs", "wiki.300.cn/gongshuyang", "gongshuyang@300.cn"))
 //                .version("1.0")
