a2b420c318480a576f853a9cbc84ded7e22415aa
GGs <122490906@qq.com>
2018-11-26 01:38:25 +0800

    first

---
+++ gateway/src/main/java/cn/ce/gateway/config/DocumentationConfig.java
@@ -0,0 +1,29 @@
+package cn.ce.gateway.config;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+import springfox.documentation.swagger.web.SwaggerResource;
+import springfox.documentation.swagger.web.SwaggerResourcesProvider;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+@Primary
+public class DocumentationConfig implements SwaggerResourcesProvider {
+    @Override
+    public List<SwaggerResource> get() {
+        List<SwaggerResource> resources = new ArrayList<>();
+        resources.add(swaggerResource("订单系统", "/feign/v2/api-docs"));
+        resources.add(swaggerResource("支付系统", "/ribbon/v2/api-docs"));
+        return resources;
+    }
+
+    private SwaggerResource swaggerResource(String name, String location) {
+        SwaggerResource swaggerResource = new SwaggerResource();
+        swaggerResource.setName(name);
+        swaggerResource.setLocation(location);
+        swaggerResource.setSwaggerVersion("2.0");
+        return swaggerResource;
+    }
+}
