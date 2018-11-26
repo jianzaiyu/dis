112f30807464470de34cd861ee8730b0e38f7f26
gongshuyang <gongshuyang@300.cn>
2018-11-26 11:47:50 +0800

    COMPANY

--- config/src/main/java/cn/ce/config/ConfigApplication.java
+++ config/src/main/java/cn/ce/config/ConfigApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
 @EnableConfigServer
 @EnableEurekaClient
+@PropertySource(value = {"classpath:bootstrap.yml"})
 public class ConfigApplication {
 
     public static void main(String[] args) {
